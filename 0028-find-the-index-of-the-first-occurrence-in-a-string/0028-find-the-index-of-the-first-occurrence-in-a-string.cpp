5311155ee4d0ebad7992e395cca4f30522719d34
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-04-11 15:13:26 -0700

    Time: 4 ms (18.68%), Space: 7.8 MB (28.65%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int strStr(string haystack, string needle) {
         if (haystack.size() < needle.size()) 
             return -1;
-        for (int i = 0; i <= haystack.size() - needle.size(); i++) {
+        for (int i = 0; i <= haystack.size(); i++) {
             if (haystack.substr(i, needle.size()) == needle) 
             return i;
         }
