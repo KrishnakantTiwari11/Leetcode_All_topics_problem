e4cb038ef32c2ca6e81c30d86fa5fa9127537d84
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-04-12 05:54:51 -0700

    Time: 0 ms (100.00%), Space: 7.5 MB (99.09%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        bool fv=false;;
+        int n=s.length()-1;
+        for(int i=n;i>=0;i--)
+        {
+            if(s[i]!=' ')
+            {
+                ans++;
+                fv=true;
+            }
+            else{
+                if(fv)
+                {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
