1466ead78655b497445ee2d0d8220a7e03c539e5
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-04-12 08:21:44 -0700

    Time: 0 ms (100.00%), Space: 10.4 MB (15.37%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size() - 1;
-        digits[n]++; // Add 1 to the last digit
+        digits[n]++;
         
-        // Carry propagation
         for (int i = n; i > 0 && digits[i] == 10; i--) {
-            digits[i] = 0; // Set the current digit to 0
-            digits[i - 1]++; // Increment the next higher-order digit
+            digits[i] = 0;
+            digits[i - 1]++;
         }
         
-        // If the first digit also becomes 10, insert 1 at the beginning
         if (digits[0] == 10) {
             digits[0] = 0;
             digits.insert(digits.begin(), 1);
