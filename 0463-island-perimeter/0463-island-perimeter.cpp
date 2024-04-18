dfd565a92089cd8da04638e7758bfeb9276e4f19
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-04-18 14:11:29 -0700

    Time: 59 ms (94.38%), Space: 100.7 MB (95.05%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -4,31 +4,31 @@ public:
     
         
         int ans=0;
+        int min=0;
+        int res=0;
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[i].size();j++)
             {
                 if(grid[i][j]==1)
                 {
-                    ans=ans+4;
-                    if(i!=0 && j!=0 && grid[i][j-1]==1 && grid[i-1][j]==1)
+                    ans=ans+1;
+                    if(j!=0 && grid[i][j-1]==1)
                     {
-                        ans=ans-4;
+                        min=min+1;
                     }
-                    else if(j!=0 && grid[i][j-1]==1)
+                    if(i!=0 && grid[i-1][j]==1)
                     {
-                        ans=ans-2;
-                    }
-                    else if(i!=0 && grid[i-1][j]==1)
-                    {
-                        ans=ans-2;
+                        min=min+1;
                     }
                     
                     
                 }
             }
         }
-        return ans;
+        res=4*ans-2*min;
+        return res;
+        
     }
         
 };
