a94b439882ce28e62a779a04c6f3db90a4137782
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-04-28 01:41:00 +0530

    Time: 3 ms (78.26%), Space: 14.2 MB (17.03%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,24 +1,20 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> freq;
-        vector<int> ans;
-
-        // Count occurrences of elements in nums1
-        for (int i = 0; i < nums1.size(); i++)
+         unordered_map<int, int> freq;
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++)
+        {
             freq[nums1[i]]++;
-
-        // Iterate through nums2 and find common elements
-        for (int i = 0; i < nums2.size(); i++) {
-            if (freq[nums2[i]] > 0) {
-                ans.push_back(nums2[i]);
-                freq[nums2[i]]--;
-            }
         }
-
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(freq[nums2[i]]>0)
+            {
+                ans.push_back(nums2[i]);
+            }
+            freq[nums2[i]]--;
+        }
         return ans;
     }
 };
