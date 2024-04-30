9a3cab16bc066fc29bcc5190e1ef0c860ac22ac4
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-05-01 02:56:02 +0530

    Time: 70 ms (24.72%), Space: 37.3 MB (27.96%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         sort(begin(nums), end(nums));
         vector<int> ans;
         for(int i = 1; i <= size(nums); i++) 
-            if(!binary_search(begin(nums), end(nums), i))
+            if(binary_search(begin(nums), end(nums), i)==false)
                 ans.push_back(i);
         return ans;
     }
