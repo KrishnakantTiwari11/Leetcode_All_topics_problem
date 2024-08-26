5b11ac59d48cb03628a3f5468a281d88821ad3df
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-08-26 11:20:04 +0530

    Time: 103 ms (91.30%) | Memory: 101.3 MB (82.67%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -5,14 +5,12 @@ public:
         int n = temperatures.size();
         vector<int> ans(n, 0);
         for (int i = n - 1; i >= 0; i--) {
-            if (st.empty()) {
-                st.push(i);
-            } else {
+            
 
                 while (!st.empty() && temperatures[i] >= temperatures[st.top()]) {
                     st.pop();
                 }
-            }
+            
 
             if (!st.empty()) {
                 ans[i] = st.top() - i;
