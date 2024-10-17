fdb43a4e1792cd01c2f1b76d6365ddc228f0e72d
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-10-17 20:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (70.34%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,43 +1,41 @@
-
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
 
 class Solution {
 public:
     int maximumSwap(int num) {
         vector<int> digits;
-
-        // Split the number into its digits
+        
         while (num > 0) {
             int digit = num % 10;
             digits.push_back(digit);
             num /= 10;
         }
         reverse(digits.begin(), digits.end());
-
         int n = digits.size();
-        // Create an array to store the last occurrence of each digit
+        
         unordered_map<int, int> last_occurrence;
-        for (int i = 0; i < n; ++i) {
-            last_occurrence[digits[i]] = i; // Store the last index of each digit
+        for (int i = 0; i < n; i++) {
+            last_occurrence[digits[i]] = i;
         }
 
-        // Try to find the maximum digit to swap with
-        for (int i = 0; i < n; ++i) {
-            // Check digits from 9 to digits[i] + 1
-            for (int d = 9; d > digits[i]; --d) {
-                if (last_occurrence.find(d) != last_occurrence.end() && last_occurrence[d] > i) {
-                    // Swap and return the result
-                    swap(digits[i], digits[last_occurrence[d]]);
-                    // Reconstruct the number
+        for (int i = 0; i < n; i++) {
+            for (int j = 9; j > digits[i]; j--) {
+                if (last_occurrence.find(j) != last_occurrence.end() && last_occurrence[j] > i) {
+                    swap(digits[i], digits[last_occurrence[j]]);
+                    
                     int reconstructed_num = 0;
                     for (int digit : digits) {
                         reconstructed_num = reconstructed_num * 10 + digit;
                     }
-                    return reconstructed_num; // Return immediately after the first swap
+                    return reconstructed_num;
                 }
             }
         }
 
-        // If no swap is made, return the original number
         int reconstructed_num = 0;
         for (int digit : digits) {
             reconstructed_num = reconstructed_num * 10 + digit;
@@ -45,5 +43,3 @@ public:
         return reconstructed_num;
     }
 };
-
-
