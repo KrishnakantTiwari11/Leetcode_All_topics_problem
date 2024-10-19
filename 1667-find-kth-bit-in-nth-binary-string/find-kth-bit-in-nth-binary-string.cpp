f7bda58e127cb9670ca26d481237e79a80f556aa
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-10-20 00:04:36 +0530

    Time: 59 ms (22.70%) | Memory: 83.8 MB (24.58%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
+    string invert(string bin_num) {
+        for (int i = 0; i < bin_num.length(); i++) {
+            if (bin_num[i] == '0') {
+                bin_num[i] = '1';
+            } else {
+                bin_num[i] = '0';
+            }
+        }
+        return bin_num;
+    }
+
+    string reverse(string bin_num) {
+        int l = 0;
+        int r = bin_num.length()-1;
+        while (l < r) {
+            swap(bin_num[l], bin_num[r]);
+            l++;
+            r--;
+        }
+        return bin_num;
+    }
     char findKthBit(int n, int k) {
-        if (n == 1) return '0';
-        int length = (1 << n) - 1;
-        int mid = length / 2 + 1;
-        if (k == mid) return '1';
-        if (k < mid) return findKthBit(n - 1, k);
-        return findKthBit(n - 1, length - k + 1) == '0' ? '1' : '0';
+        // if(n==1 && k==1)
+        // {
+        //     return '0';
+        // }
+        string str = "0";
+        string fin = "";
+        n=n-1;
+        k=k-1;
+        for (int i = 0; i < n; i++) {
+            fin = str + "1" + reverse(invert(str));
+            str = fin;
+        }
+        return str[k];
     }
 };
