b4c2571d822e4ec04743f3ddf16b47235a8d115c
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-11 23:18:49 +0530

    Time: 43 ms (84.29%) | Memory: 105.1 MB (51.16%) - LeetSync

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int l = 0, res = 0;
+        
+        for (int r = 0; r < n; r++) {
+            while (nums[r] - nums[l] > 2 * k) {
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
