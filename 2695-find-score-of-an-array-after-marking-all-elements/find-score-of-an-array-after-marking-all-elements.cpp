5c22d10430cdc7f92656da394f757781a6297593
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-14 01:23:45 +0530

    Time: 219 ms (28.77%) | Memory: 129.4 MB (30.50%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
-      struct Compare {
-    bool operator()(pair<int, int> a, pair<int, int> b) {
-        return a > b; 
-    }
-};
-
+    struct Compare {
+        bool operator()(pair<int, int> a, pair<int, int> b) {
+            return a > b; 
+        }
+    };
 
     long long findScore(vector<int>& nums) {
-         priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
-         set<int>marked;
-         int n=nums.size();
-         long long score=0;
-         for(int i=0;i<n;i++)
-         {
-            pq.push({nums[i],i});
-         }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        unordered_map<int, int> marked;
+        int n = nums.size();
+        long long score = 0;
 
-         while(!pq.empty())
-         {
-                pair<int, int> top = pq.top();
-                pq.pop();
-                if(!marked.empty() &&  marked.find(top.second)!=marked.end())
-                {
-                    continue;
-                }
-                score+=top.first;
-                if(top.second==0)
-                {
-                    marked.insert(1);
-                }
-                else if(top.second==n-1)
-                {
-                    marked.insert(n-2);
-                }
-                else
-                {
-                    marked.insert(top.second-1);
-                    marked.insert(top.second+1);
-                }
-         }
-         return score;
+        for (int i = 0; i < n; i++) {
+            pq.push({nums[i], i});
+        }
+
+        while (!pq.empty()) {
+            pair<int, int> top = pq.top();
+            pq.pop();
+
+            if (marked.count(top.second)) {
+                continue;
+            }
+
+            score += top.first;
+
+            if (top.second == 0) {
+                marked[1] = 1;
+            } else if (top.second == n - 1) {
+                marked[n - 2] = 1;
+            } else {
+                marked[top.second - 1] = 1;
+                marked[top.second + 1] = 1;
+            }
+        }
+
+        return score;
     }
 };
