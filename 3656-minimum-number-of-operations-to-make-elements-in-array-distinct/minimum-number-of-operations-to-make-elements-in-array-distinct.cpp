4f1b8231e73a58d8ac308048628f86030e6793d2
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-22 11:22:21 +0530

    Time: 12 ms (18.75%) | Memory: 28.3 MB (43.75%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -3,17 +3,30 @@ public:
     int minimumOperations(vector<int>& nums) {
         int i = 0;
         int count = 0;
+        map<int, int> temp;
+        
+        for (int num : nums) {
+            temp[num]++;
+        }
 
         while (true) {
-            unordered_set<int> temp(nums.begin() + i, nums.end());
             if (temp.size() == nums.size() - i) {
                 return count;
             }
-            if(i+3>=nums.size()-1)
-            {
+            if(i + 3 >= nums.size() - 1) {
                 count++;
                 return count;
             }
+            
+            temp[nums[i]]--;
+            if (temp[nums[i]] == 0) temp.erase(nums[i]);
+            
+            temp[nums[i+1]]--;
+            if (temp[nums[i+1]] == 0) temp.erase(nums[i+1]);
+            
+            temp[nums[i+2]]--;
+            if (temp[nums[i+2]] == 0) temp.erase(nums[i+2]);
+
             i += 3;
             count++;
         }
