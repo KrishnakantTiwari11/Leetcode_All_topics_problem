7d095ab7d7f252e48e0a32c4ae70030606d8c473
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-23 19:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (24.32%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,39 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if(root==NULL)
-        {
-            return {};
-        }
-        stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
-        st1.push(root);
-        while (!st1.empty()) {
-            TreeNode* node = st1.top();
-            st1.pop();
-            st2.push(node);
-            if (node->left)
-                st1.push(node->left);
-            if (node->right)
-                st1.push(node->right);
-        }
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
         vector<int> res;
-        while (!st2.empty()) {
-            TreeNode* node = st2.top();
-            st2.pop();
-            res.push_back(node->val);
+        TreeNode* temp;
+
+        while (curr != NULL || !st.empty()) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    res.push_back(temp->val);
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        res.push_back(temp->val);
+                    }
+                } else {
+                    curr = temp;
+                }
+            }
         }
 
         return res;
