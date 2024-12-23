4302b6194cfefda3745078b594128e7a1d7216c4
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-23 12:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (29.11%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int numTrees(int n) {
         long long res = 1;
-        for (int i = 0; i < n; i++) {
-            res = res * (2 * n - i) / (i + 1);
+        for (int i = 1; i <= n; i++) {
+            res = res * (2 * n - i + 1) / i;
         }
         return res / (n + 1);
     }
