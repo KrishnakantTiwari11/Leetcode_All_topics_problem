c5140ec86c058e294dcdfc60494d3dbbf3925157
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-27 20:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (13.39%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,20 +11,24 @@
  */
 class Solution {
 public: 
-    bool ans=true;
     int helper(TreeNode*root){
         if(root==NULL){
             return 0;
         }
         int left=helper(root->left);
+        if(left==-1){
+            return -1;
+        }
         int right=helper(root->right);
+        if(right==-1){
+            return -1;
+        }
         if(abs(left-right)>1){
-            ans=false;
+            return -1;
         }
         return max(left,right)+1;
     }
     bool isBalanced(TreeNode* root) {
-        helper(root);
-        return ans;
+        return helper(root)!=-1;
     }
 };
