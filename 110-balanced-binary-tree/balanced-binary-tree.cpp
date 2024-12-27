a7fb87c270c831ae5cbccbe9e2ef2bd8c95d6db6
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-27 20:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (49.15%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,28 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-public:
-    bool ans;
-    int find(TreeNode* root) {
-        if (root == NULL)
+public: 
+    bool ans=true;
+    int helper(TreeNode*root){
+        if(root==NULL){
             return 0;
-        if (!ans)
-            return 0;
-        int maxleft = find(root->left);
-        int maxright = find(root->right);
-        if (abs(maxleft - maxright) > 1) {
-            ans = false;
         }
-        return max(maxleft, maxright) + 1;
+        int left=helper(root->left);
+        int right=helper(root->right);
+        if(abs(left-right)>1){
+            ans=false;
+        }
+        return max(left,right)+1;
     }
     bool isBalanced(TreeNode* root) {
-        ans = true;
-        int temp = find(root);
+        helper(root);
         return ans;
     }
 };
