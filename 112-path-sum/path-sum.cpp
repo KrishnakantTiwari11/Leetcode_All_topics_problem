275e0b3339689247ed00a2b8737b5e5d0ded3c0e
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2024-12-28 13:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (45.57%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,34 +11,27 @@
  */
 class Solution {
 public:
+    bool res=false;
+    void helper(TreeNode*node,int target){
+        if(!node->left && !node->right){
+            if(node->val==target){
+                res=true;
+            }
+        }
+        if(node->left){
+            node->left->val+=node->val;
+            helper(node->left,target);
+        }
+        if(node->right){
+            node->right->val+=node->val;
+            helper(node->right,target);
+        }
+    }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==NULL){
             return 0;
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<vector<int>>res;
-        while(!q.empty()){
-            int n=q.size();
-            vector<int>level;
-            for(int i=0;i<n;i++){
-                TreeNode*node=q.front();
-                q.pop();
-                if(!node->left && !node->right){
-                    if(node->val==targetSum)return true;
-                }
-                level.push_back(node->val);
-                if(node->left){
-                    node->left->val+=node->val;
-                    q.push(node->left);
-                }
-                if(node->right){
-                    node->right->val+=node->val;
-                    q.push(node->right);
-                }
-            }
-            res.push_back(level);
-        }
-        return false;
+        helper(root,targetSum);
+        return res;
     }
 };
