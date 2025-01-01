82bb4d66d8bbd0151f8c411f081edf22dc09423a
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-01 13:20:02 +0530

    Time: 284 ms (65.06%) | Memory: 15.6 MB (85.80%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         int n=arr.size();
         int res=0;
         unordered_set<int>mp(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
                 int a=arr[i],b=arr[j],l=2;
                 while(mp.count(a+b)){
