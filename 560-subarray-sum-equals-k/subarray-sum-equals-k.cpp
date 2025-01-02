164704424891a4b479665e8632a1463f46ab6296
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-02 16:43:27 +0530

    Time: 1427 ms (23.36%) | Memory: 34.4 MB (82.82%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>prefix(n);
-        int ans=0;
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=nums[i]+prefix[i-1];
-        }
-        unordered_map<int,int>mp;
-        for(auto ch:prefix){
-            if(ch==k){
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size(); // taking the size of the array
+        
+        int ans = 0; // ans variable to store our count
+        
+        for(int i = 0; i < n; i++) // traverse from the array
+        {
+            int sum = arr[i]; // provide sum with arr[i]
+            
+            if(sum == k) // if element itself equal to k, then also increment count
                 ans++;
+            
+            for(int j = i + 1; j < n; j++) // now moving forward,
+            {
+                sum += arr[j]; // add elements with sum
+                
+                if(sum == k) // if at any point they become equal to k
+                    ans++; // increment answer
             }
-            if(mp.find(ch-k)!=mp.end()){
-                ans+=mp[ch-k];
-            }
-            mp[ch]++;
+            
         }
-        return ans;
+        
+        return ans; // and at last, return answer
     }
 };
