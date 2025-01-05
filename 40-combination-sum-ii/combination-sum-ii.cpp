b1750381ed36237a5cc4763a6042e3335d5c6f72
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-05 22:35:21 +0530

    Time: 4 ms (28.24%) | Memory: 14.2 MB (34.34%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void cs(vector<vector<int>>& res, int target, vector<int>& temp,
-            vector<int>& nums, int i,int sum) {
-        if (sum==target) {
-            res.push_back(temp);
-            return;
-        }
-        if (i >= nums.size() || sum>target) {
-            return;
-        }
-       for(int j=i;j<nums.size();j++){
-            if(j>i && nums[j]==nums[j-1]){
-                continue;
+    void cs(vector<vector<int>>& res, vector<int>& temp, int ind, int sum,
+            int target,vector<int>&nums) {
+                if(sum==target){
+                    res.push_back(temp);
+                    return;
+                }
+                if(sum>target || ind>=nums.size())return;
+                for(int i=ind;i<nums.size();i++){
+                    if(i>ind && nums[i]==nums[i-1]){
+                        continue;
+                    }
+                    sum+=nums[i];
+                    temp.push_back(nums[i]);
+                    cs(res,temp,i+1,sum,target,nums);
+                    temp.pop_back();
+                    sum-=nums[i];
+                }
             }
-            temp.push_back(nums[j]);
-            cs(res,target,temp,nums,j+1,sum+nums[j]);
-            temp.pop_back();
-       }
-    }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-          sort(candidates.begin(),candidates.end());
-          vector<vector<int>>res;
-          vector<int>temp;
-          cs(res,target,temp,candidates,0,0);
-          return res;
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>res;
+        vector<int>temp;
+        cs(res,temp,0,0,target,candidates);
+        return res;
     }
 };
