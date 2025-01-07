010c34c6e5e9e84f0d12e978195c4b4d99f9f837
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-07 14:31:28 +0530

    Time: 2064 ms (5.02%) | Memory: 373.3 MB (5.61%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& res, vector<int>& temp,
-                vector<int>& visited, vector<int>& nums,int & k) {
-        if (temp.size() == nums.size()) {
+   bool helper(vector<vector<int>>&res,vector<int>&temp,vector<int>&visited,vector<int> & nums,int & k){
+        if(temp.size()==nums.size()){
             res.push_back(temp);
             k--;
-            return k;
+            if(k==0)return true;
+            return false;
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (!visited[i]) {
-                temp.push_back(nums[i]);
-                visited[i] = 1;
-                if(helper(res, temp, visited, nums,k)==0){
-                    return k;
-                }
-                visited[i] = 0;
-                temp.pop_back();
-            }
+        for(int i=0;i<nums.size();i++){
+           if(!visited[i]){
+             temp.push_back(nums[i]);
+             visited[i]=1;
+             if(helper(res,temp,visited,nums,k))return true;
+             temp.pop_back();
+             visited[i]=0;
+           }
         }
-        return k;
+        return false;
     }
     string getPermutation(int n, int k) {
-        vector<int> nums(n);
+         vector<int> nums(n);
         vector<int>visited(n,0);
         vector<vector<int>>res;
         vector<int>temp;
