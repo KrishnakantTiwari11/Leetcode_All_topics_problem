cff79f822d2708ba990104ec2a4fba8e51a19219
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-08 14:20:39 +0530

    Time: 2 ms (76.24%) | Memory: 10.2 MB (57.44%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    void solve(vector<vector<string>>&ans,vector<string>&temp,int n,int col,vector<int>&left_row,vector<int>& up_diag,vector<int>& l_diag){
-        if(col>=n){
-            ans.push_back(temp);
+    void solve(vector<vector<string>>& res, vector<string>& temp, int col, int n,vector<int> & left_row,vector<int> & up_left,vector<int>& down_right) {
+        if (col >= n) {
+            res.push_back(temp);
             return;
         }
-        for(int row=0;row<n;row++){
-             if(left_row[row]==0 && l_diag[row+col]==0 && up_diag[n-1+(col-row)]==0){
-                temp[row][col]='Q';
-                left_row[row]=1;
-                l_diag[row+col]=1;
-                up_diag[n-1+(col-row)]=1;
-                solve(ans,temp,n,col+1,left_row,up_diag,l_diag);
-                temp[row][col]='.';
-                left_row[row]=0;
-                l_diag[row+col]=0;
-                up_diag[n-1+(col-row)]=0;
-             }
+        for (int row = 0; row < n; row++) {
+            if (left_row[row] == 0 && up_left[row + col] == 0 &&
+                down_right[n - 1 + col - row] == 0) {
+                temp[row][col] = 'Q';
+                left_row[row] = 1;
+                up_left[row + col] = 1;
+                down_right[n - 1 + col - row] = 1;
+                solve(res, temp, col+1, n,left_row,up_left,down_right);
+                left_row[row] = 0;
+                up_left[row + col] = 0;
+                down_right[n - 1 + col - row] = 0;
+                temp[row][col] = '.';
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
         vector<string>temp(n,string(n,'.'));
+        vector<vector<string>>res;
         vector<int>left_row(n);
-        vector<int>up_diag(2*n-1);
-        vector<int>l_diag(2*n-1);
-        solve(ans,temp,n,0,left_row,up_diag,l_diag);
-        return ans;
+        vector<int>up_left(2*n-1);
+        vector<int>down_right(2*n-1);
+        solve(res,temp,0,n,left_row,up_left,down_right);
+        return res;
     }
 };
