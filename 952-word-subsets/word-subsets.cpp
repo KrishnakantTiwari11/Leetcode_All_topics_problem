41e98b05c37bca01a0e7c418b05884fe4452202a
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-10 18:37:43 +0530

    Time: 57 ms (58.90%) | Memory: 75.3 MB (72.15%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<int> maxFreq(26, 0); 
-    
-        for (const auto& word : words2) {
-            vector<int> freq(26, 0);
-            for (char ch : word) {
-                freq[ch - 'a']++;
+        vector<char>maxFreq(26,0);
+        vector<string>ans;
+        for(auto word2:words2){
+            vector<char>freq(26,0);
+            for(auto ch:word2){
+                freq[ch-'a']++;
             }
-            for (int i = 0; i < 26; ++i) {
-                maxFreq[i] = max(maxFreq[i], freq[i]);
+            for(int i=0;i<26;i++){
+                maxFreq[i]=max(maxFreq[i],freq[i]);
             }
         }
-
-        vector<string> result;
-    
-        for (const auto& word : words1) {
-            vector<int> freq(26, 0);
-            for (char ch : word) {
-                freq[ch - 'a']++;
+        for(auto word1:words1){
+            vector<char>freq(26,0);
+            for(auto ch:word1){
+                freq[ch-'a']++;
             }
-            
-            bool isValid = true;
-            for (int i = 0; i < 26; ++i) {
-                if (freq[i] < maxFreq[i]) {
-                    isValid = false;
+            bool isValid=true;
+            for(int i=0;i<26;i++){
+                if(freq[i]<maxFreq[i]){
+                    isValid=false;
                     break;
                 }
             }
-            if (isValid) {
-                result.push_back(word);
+            if(isValid){
+                ans.push_back(word1);
             }
         }
-        
-        return result;
+        return ans;
     }
 };
