ee6d10367121b3129a8203dac8098c8bee900e71
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-15 16:16:40 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (7.94%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,48 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (!root)
-            return true;
-        if (!root->left && !root->right)
-            return true;
-        if (!root->left || !root->right)
-            return false;
+        if (!root) return true;
+
         queue<pair<TreeNode*, TreeNode*>> q;
         q.push({root->left, root->right});
-        while (!q.empty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                pair<TreeNode*, TreeNode*> node = q.front();
-                q.pop();
-                if (node.first->val != node.second->val)
-                    return false;
-                if (node.first->left && !node.second->right)
-                    return false;
-                if (!node.first->left && node.second->right)
-                    return false;
-                if (node.first->right && !node.second->left)
-                    return false;
-                if (!node.first->right && node.second->left)
-                    return false;
 
-                if (node.first->left && node.second->right)
-                    q.push({node.first->left, node.second->right});
-                if (node.first->right && node.second->left)
-                    q.push({node.first->right, node.second->left});
-            }
+        while (!q.empty()) {
+            auto [left, right] = q.front();
+            q.pop();
+
+            if (!left && !right) continue;
+            if (!left || !right || left->val != right->val) return false;
+
+            q.push({left->left, right->right});
+            q.push({left->right, right->left});
         }
+
         return true;
     }
 };
