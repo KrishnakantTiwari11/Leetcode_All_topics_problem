f394420c6dead1a9e244ef0eb4b342fb3545b0ae
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-15 16:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (21.54%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,22 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        if (!root) return true;
-
-        queue<pair<TreeNode*, TreeNode*>> q;
-        q.push({root->left, root->right});
-
-        while (!q.empty()) {
-            auto [left, right] = q.front();
-            q.pop();
-
-            if (!left && !right) continue;
-            if (!left || !right || left->val != right->val) return false;
-
-            q.push({left->left, right->right});
-            q.push({left->right, right->left});
-        }
-
+   bool helper(TreeNode*node1,TreeNode*node2){
+        if(!node1 && !node2)return true;
+        if(!node1 || !node2)return false;
+        if(node1->val!=node2->val)return false;
+        if(!helper(node1->left,node2->right))return false;
+        if(!helper(node1->right,node2->left))return false;
         return true;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return helper(root->left,root->right);
     }
 };
