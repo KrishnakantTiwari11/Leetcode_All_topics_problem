65c32f415313714f8b8a2477db3d9d698e19115a
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-23 19:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (64.65%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,42 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root==NULL){
-            return {};
-        }
-        stack<pair<TreeNode*,int>>st;
-        vector<int>preorder;
-        vector<int>inorder;
-        vector<int>postorder;
-        st.push({root,1});
-        while(!st.empty())
-        {
-            TreeNode*node=st.top().first;
-            int num=st.top().second;
-            st.pop();
-            if(num==1){
-                preorder.push_back(node->val);
-                num++;
-                st.push({node,num});
-                if(node->left)st.push({node->left,1});
-            }
-            else if(num==2){
-                inorder.push_back(node->val);
-                num++;
-                st.push({node,num});
-                if(node->right)st.push({node->right,1});
-            }
-            else{
-                postorder.push_back(node->val);
+        vector<int> ans;
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* temp = curr->left;
+                while (temp->right != NULL && temp->right != curr) {
+                    temp = temp->right;
+                }
+                if (temp->right == NULL) {
+                    temp->right = curr;
+                    curr = curr->left;
+                } else {
+                    temp->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
         }
-        return inorder;
-
+        return ans;
     }
 };
