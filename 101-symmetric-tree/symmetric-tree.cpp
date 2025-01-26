d04e8b458d2a5bcc3375fcda792dd8e092636077
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-01-26 22:47:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (84.65%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
-   bool helper(TreeNode*node1,TreeNode*node2){
-        if(!node1 && !node2)return true;
-        if(!node1 || !node2)return false;
-        if(node1->val!=node2->val)return false;
-        if(!helper(node1->left,node2->right))return false;
-        if(!helper(node1->right,node2->left))return false;
+    bool helper(TreeNode*p,TreeNode*q){
+        if(!p && !q)return true;
+        if(!p || !q)return false;
+        if(p->val!=q->val)return false;
+        if(!helper(p->left,q->right))return false;
+        if(!helper(p->right,q->left))return false;
         return true;
     }
     bool isSymmetric(TreeNode* root) {
+        if(!root)return true;
         return helper(root->left,root->right);
     }
 };
