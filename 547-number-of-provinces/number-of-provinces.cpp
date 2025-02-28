dae7b125ea8d040b97fd6c4c9c7353d2bd6d0205
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-02-28 22:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (73.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    void dfs(int start,vector<int>&visited,vector<vector<int>>& isConnected){
-          visited[start]=1;
-          for(int i=0;i<isConnected.size();i++){
-            if(isConnected[start][i]==1 && visited[i]==0){
-                dfs(i,visited,isConnected);
-            }
-          }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int res=0;
-        int n=isConnected.size();
-        vector<int>visited(n,0);
-        for(int i=0;i<n;i++){
-            if(visited[i]==0){
-                res++;
-                dfs(i,visited,isConnected);
+    void helper(vector<vector<int>>& isConnected, vector<int>& visited, int start) {
+        visited[start] = 1; // Mark the current node as visited
+        for (int ch = 0; ch < isConnected.size(); ++ch) {
+            if (isConnected[start][ch] == 1 && !visited[ch]) {
+                helper(isConnected, visited, ch);
             }
         }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> visited(n, 0);
+        int res = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                res++; // Found a new connected component
+                helper(isConnected, visited, i); // Explore the entire component
+            }
+        }
+        
         return res;
     }
 };
