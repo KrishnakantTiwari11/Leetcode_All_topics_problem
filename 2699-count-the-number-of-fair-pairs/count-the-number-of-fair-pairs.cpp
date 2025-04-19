869fda0e8616f84e51ff1d277f6212ba9a0fe59d
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-04-20 00:10:55 +0530

    Time: 29 ms (86.17%) | Memory: 60.4 MB (38.74%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    long long countAtLeast(vector<int>& nums, long long comp) {
-        long long ans = 0;
-        int i = 0, j = nums.size() - 1;
-        while (i < j) {
-            if ((long long)nums[i] + nums[j] >= comp) {
-                ans += j - i;
+    long long helper(vector<int>&nums,int comp){
+        int j=nums.size()-1,i=0;
+        long long res=0;
+        while(i<j){
+            if(nums[i]+nums[j]>=comp){
+                res+=j-i;
                 j--;
-            } else {
+            }
+            else{
                 i++;
             }
         }
-        return ans;
+        return res;
     }
-
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        sort(nums.begin(), nums.end());
-        return countAtLeast(nums, lower) - countAtLeast(nums, (long long)upper + 1);
+        sort(nums.begin(),nums.end());
+        return helper(nums,lower)-helper(nums,(long long)upper+1);
     }
 };
