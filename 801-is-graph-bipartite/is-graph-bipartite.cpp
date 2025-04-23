1ef9b647d732e8aa9f6c2fe242c1bc6257cfd037
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-04-23 19:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (80.07%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool helper(vector<vector<int>>& graph,vector<int>&visited,int curr,int par){
-            if(par!=-1){
-                visited[curr]=-visited[par];
+        if(par==-1){
+            visited[curr]=1;
+        }
+        else{
+            visited[curr]=-visited[par];
+        }
+        for(auto ch:graph[curr]){
+            if(!visited[ch]){
+                if(!helper(graph,visited,ch,curr))return false;
             }
-            else{
-                visited[curr]=1;
+            else if(visited[curr]==visited[ch]){
+                return false;
             }
-            for(auto ch:graph[curr]){
-                if(visited[ch]==0){
-                    if(!helper(graph,visited,ch,curr))return false;
-                }
-                else if(visited[ch]==visited[curr]){
-                    return false;
-                }
-            }
+        }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>visited(n,0);
         for(int i=0;i<n;i++){
-            if(visited[i]==0){
-               if(!helper(graph,visited,i,-1))return false;
+            if(!visited[i]){
+                if(!helper(graph,visited,i,-1))return false;
             }
         }
         return true;
