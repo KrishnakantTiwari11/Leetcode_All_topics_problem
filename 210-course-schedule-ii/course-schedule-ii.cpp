f576fa149f82aa29c082c5bc5aa1c55e13a2ce3a
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-04-26 15:55:45 +0530

    Time: 6 ms (41.61%) | Memory: 19.1 MB (20.05%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    bool helper(vector<vector<int>>& graph,vector<int>&visited,vector<int>&path,int curr,vector<int>&ans){
-        visited[curr]=1;
-        path[curr]=1;
-        for(auto ch:graph[curr]){
-            if(!visited[ch]){
-                if(helper(graph,visited,path,ch,ans))return true;
-            }
-            else if(path[ch]){
+    bool helper(vector<vector<int>>& mat, vector<int>& visited,
+                vector<int>& path, vector<int>& res, int curr) {
+        visited[curr] = 1;
+        path[curr] = 1;
+        for (auto neigh : mat[curr]) {
+            if (!visited[neigh]) {
+                if (helper(mat, visited, path, res, neigh))
+                    return true;
+            } else if (path[neigh]) {
                 return true;
             }
         }
-        path[curr]=0;
-        ans.push_back(curr);
+        path[curr] = 0;
+        res.push_back(curr);
         return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>ans,visited(numCourses,0),path(numCourses,0);
-        vector<vector<int>>mat(numCourses);
-        for(auto ch:prerequisites){
-            int u=ch[0];
-            int v=ch[1];
+        int n = numCourses;
+        vector<int> visited(n, 0), path(n, 0), res;
+        vector<vector<int>> mat(n);
+        for (auto edge : prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
             mat[v].push_back(u);
         }
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i]){
-                if(helper(mat,visited,path,i,ans))return {};
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                if (helper(mat, visited, path,res, i))
+                    return {};
             }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
