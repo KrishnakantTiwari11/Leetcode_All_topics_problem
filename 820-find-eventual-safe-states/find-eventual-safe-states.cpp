ef6e0aa9b2113ae7f4aa613ee270ab260fc504a0
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-02 03:03:33 +0530

    Time: 59 ms (20.02%) | Memory: 66.4 MB (14.45%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,29 +1,39 @@
 class Solution {
-    bool dfs(int i, vector<vector<int>>& graph, unordered_map<int, bool>& safe) {
-        if (safe.find(i) != safe.end()) {
-            return safe[i];
-        }
-        safe[i] = false;
-        for (auto it : graph[i]) {
-            if (!dfs(it, graph, safe)) {
-                return false;
-            }
-        }
-        safe[i] = true;
-        return true;
-    }
-
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        unordered_map<int, bool> safe;
-        vector<int> res;
-
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, safe)) {
-                res.push_back(i);
+    bool dfs(vector<vector<int>>&adj,int src,vector<bool>&vis,vector<bool>&recst){
+        vis[src]=true;
+        recst[src]=true;
+        for(auto x:adj[src]){
+            if(vis[x]==false && dfs(adj,x,vis,recst)){
+                return true;
+            }
+            else if(recst[x]==true){
+            return true;
             }
         }
-        return res;
+        recst[src]=false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+         vector<vector<int>>adj(n);
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+            vector<bool>vis(n,false);
+        vector<bool>recst(n,false);
+        for(int i=0;i<n;i++){
+            if(vis[i]==false){
+                dfs(adj,i,vis,recst);
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<recst.size();i++){
+          if(!recst[i])
+          ans.push_back(i);
+        }
+        return ans;
     }
 };
