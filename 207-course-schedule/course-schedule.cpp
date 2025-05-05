36f5cc1f0c2a5dbe9c5dc2786ae4d4068a3a4ef5
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-05 13:13:26 +0530

    Time: 4 ms (70.53%) | Memory: 20.3 MB (26.48%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool helper(vector<vector<int>>&mat,vector<int>&visited,vector<int>&path,int curr){
-        visited[curr]=1;
+    bool helper(vector<vector<int>>&mat,vector<int>&node,vector<int>&path,int curr){
         path[curr]=1;
-        for(auto neigh:mat[curr]){
-            if(!visited[neigh]){
-                if(!helper(mat,visited,path,neigh))return false;
+        node[curr]=1;
+        for(auto ch:mat[curr]){
+            if(!node[ch]){
+                if(!helper(mat,node,path,ch))return false;
             }
-            else if(path[neigh]){
+            else if(path[ch]){
                 return false;
             }
         }
@@ -16,16 +16,17 @@ public:
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
-        vector<int>visited(n,0),path(n,0);
         vector<vector<int>>mat(n);
-        for(auto edge:prerequisites){
-            int u=edge[0];
-            int v=edge[1];
+        for(auto pre:prerequisites){
+            int u=pre[0];
+            int v=pre[1];
             mat[v].push_back(u);
         }
+        vector<int>node(n,0);
+        vector<int>path(n,0);
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                if(!helper(mat,visited,path,i))return false;
+            if(!node[i]){
+                if(!helper(mat,node,path,i))return false;
             }
         }
         return true;
