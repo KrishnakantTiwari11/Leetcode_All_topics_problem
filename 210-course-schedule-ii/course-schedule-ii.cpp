6c1b258537e1425d7de78e0e3415ab26e404e420
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-08 15:15:48 +0530

    Time: 11 ms (13.60%) | Memory: 19.3 MB (14.19%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<vector<int>>mat(n);
-        for(auto pre:prerequisites){
-            int u=pre[0];
-            int v=pre[1];
+        int n = numCourses;
+        vector<vector<int>> mat(n);
+        queue<int> q;
+        vector<int> inDegree(n, 0), res;
+        
+        //Converting pair into adjacency list
+        for (auto pre : prerequisites) {
+            int u = pre[0];
+            int v = pre[1];
             mat[v].push_back(u);
         }
-        vector<int>inDegree(n);
-        for(auto vec:mat){
-            for(int i=0;i<vec.size();i++){
-                inDegree[vec[i]]++;
+
+        //Calculating InDegree for each node
+        for (auto ch : mat) {
+            for (int i = 0; i < ch.size(); i++) {
+                inDegree[ch[i]]++;
             }
         }
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==0){
+
+        //Pushing node with InDegree==0 in the queue
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
                 q.push(i);
             }
         }
-        vector<int>res;
-        while(!q.empty()){
-            int curr=q.front();
+        while (!q.empty()) {
+            int curr = q.front();
             q.pop();
             res.push_back(curr);
-            for(auto ch:mat[curr]){
+            for (auto ch : mat[curr]) {
                 inDegree[ch]--;
-                if(inDegree[ch]==0){
+                if (inDegree[ch] == 0) {
                     q.push(ch);
                 }
             }
         }
-        if(res.size()==n)return res;
+        if (res.size() == n)
+            return res;
         return {};
     }
 };
