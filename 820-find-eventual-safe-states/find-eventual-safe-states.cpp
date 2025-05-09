7106f57fc67375dfbeb90a384b68988ff9c606e8
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-09 16:29:56 +0530

    Time: 47 ms (54.09%) | Memory: 66.3 MB (23.01%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj,int src,vector<bool>&vis,vector<bool>&recst){
-        vis[src]=true;
-        recst[src]=true;
-        for(auto x:adj[src]){
-            if(vis[x]==false && dfs(adj,x,vis,recst)){
-                return true;
-            }
-            else if(recst[x]==true){
-            return true;
-            }
-        }
-        recst[src]=false;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-         vector<vector<int>>adj(n);
-        for(int i=0;i<graph.size();i++){
-            for(int j=0;j<graph[i].size();j++){
-                adj[i].push_back(graph[i][j]);
-            }
-        }
-            vector<bool>vis(n,false);
-        vector<bool>recst(n,false);
+        vector<vector<int>>mat(n);
+        vector<int>inDegree(n,0),res;
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
-                dfs(adj,i,vis,recst);
+            for(int j=0;j<graph[i].size();j++){
+                int u=i;
+                int v=graph[i][j];
+                mat[v].push_back(u);
+                inDegree[u]++;
             }
         }
-        vector<int>ans;
-        for(int i=0;i<recst.size();i++){
-          if(!recst[i])
-          ans.push_back(i);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(inDegree[i]==0)q.push(i);
         }
-        return ans;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            res.push_back(curr);
+            for(auto ch:mat[curr]){
+                inDegree[ch]--;
+                if(inDegree[ch]==0)q.push(ch);
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
     }
 };
