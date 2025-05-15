ac1dc7f3fe0c23700e5339936b247fd68ba4c2d4
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-15 16:47:27 +0530

    Time: 835 ms (26.73%) | Memory: 466.9 MB (44.49%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,34 +1,24 @@
-#include <vector>
-#include <string>
-
 class Solution {
 public:
-    int lengthAfterTransformations(std::string s, int t) {
-        const int MOD = 1000000007;
-        std::vector<int> cnt(26, 0);
-
-        for (char c : s) {
-            cnt[c - 'a']++;
+    int lengthAfterTransformations(string s, int t) {
+        const int MOD = 1e9 + 7;
+        vector<long long> freq(26, 0);
+        for (char ch : s) {
+            freq[ch - 'a']++;
         }
-
-        for (int j = 0; j < t; j++) {
-            std::vector<int> tmp(26, 0);
-            for (int i = 0; i < 26; i++) {
-                if (i == 25) {
-                    tmp[0] = (tmp[0] + cnt[i]) % MOD;
-                    tmp[1] = (tmp[1] + cnt[i]) % MOD;
-                } else {
-                    tmp[i + 1] = (tmp[i + 1] + cnt[i]) % MOD;
-                }
+        for (int i = 0; i < t; i++) {
+            vector<long long> temp(26, 0);
+            for (int j = 0; j < 25; j++) {
+                temp[j + 1] = (temp[j + 1] + freq[j]) % MOD;
             }
-            cnt = tmp;
+            temp[0] = (freq[25]) % MOD;
+            temp[1] = (temp[1] + freq[25]) % MOD;
+            freq = temp;
         }
-
-        int len = 0;
-        for (int c : cnt) {
-            len = (len + c) % MOD;
+        long long res = 0;
+        for (auto ch : freq) {
+            res = (res + ch) % MOD;
         }
-
-        return len;
+        return (int)res;
     }
 };
