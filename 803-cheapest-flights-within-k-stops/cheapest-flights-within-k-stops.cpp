2aba7db97c7312bf5a126eabbd8d9d1ec02b2e21
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-21 02:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.64%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-
-   
-
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-
-        vector<vector<pair<int,int>>>adj(n);
-        for(int i=0; i<flights.size(); i++){
-            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]}); // adjNode,edgeWeight
-        }
-
-        vector<int>dist(n,INT_MAX);
-        dist[src] =0;
-        // priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
-        queue< pair<int,pair<int,int>> > pq;
-        pq.push({0,{0,src}});
-
-        while(pq.size()>0){
-            auto p = pq.front();
-            pq.pop();
-            int d = p.first;
-            int stops = p.second.first;
-            int node = p.second.second;
-
-            if(stops>k) continue;
-
-            for(auto it: adj[node]){
-                int adjNode = it.first;
-                int edgeWt = it.second;
-                int newDist = d + edgeWt;
-
-                if(newDist < dist[adjNode]){
-                //    if(adjNode==dst && stops>k) continue;
-                   dist[adjNode] = newDist;
-                   pq.push({newDist,{stops+1,adjNode}});
-                }
-            }
-        }
-     
+        vector<int> distance(n, INT_MAX);
         
-        if(dist[dst]==INT_MAX) return -1;
-       
-        return dist[dst];
-
-
+        unordered_map<int, vector<pair<int, int>>> adj;
+        
+        for(vector<int> &vec : flights) {
+            int u    = vec[0];
+            int v    = vec[1];
+            int cost = vec[2];
+            
+            adj[u].push_back({v, cost});
+        }
+        
+        queue<pair<int, int>> que;
+        que.push({src, 0});
+        distance[src] = 0;
+        
+        int level = 0;
+        
+        while(!que.empty() && level <= k) {
+            int N = que.size();
+            
+            while(N--) {
+                int u = que.front().first;
+                int d = que.front().second;
+                que.pop();
+                
+                for(pair<int, int> &P : adj[u]) {
+                    
+                    int v    = P.first;
+                    int cost = P.second;
+                    
+                    if(distance[v] > d + cost) {
+                        distance[v] = d + cost;
+                        que.push({v, d+cost});
+                    }
+                    
+                }
+                
+            }
+            level++;
+        }
+        
+        return distance[dst] == INT_MAX ? -1 : distance[dst];
     }
 };
