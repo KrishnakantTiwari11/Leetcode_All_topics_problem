15e7a33a78cb5261904fc4daaaf8f784ed9ad4e8
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-05-30 01:14:40 +0530

    Time: 75 ms (88.66%) | Memory: 47.9 MB (32.57%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>>mat(n+1);
-        for(auto edge:times){
-            int u=edge[0],v=edge[1],w=edge[2];
+        for(auto time:times){
+            int u = time[0],v=time[1],w=time[2];
             mat[u].push_back({v,w});
         }
-        vector<int> distance(n+1, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            q;
-        distance[k] = 0;
-        q.push({0, k});
-        while (!q.empty()){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        vector<int>distance(n+1,INT_MAX);
+        q.push({0,k});
+        distance[k]=0;
+        while(!q.empty()){
             int curr = q.top().second;
-            int dist = q.top().first;
+            int max_dist = q.top().first;
             q.pop();
             for(auto neigh:mat[curr]){
-                int next = neigh.first;
-                int wt = neigh.second;
-                
+                int wt = neigh.second , next = neigh.first;
                 if(distance[curr] + wt < distance[next]){
                     distance[next] = distance[curr] + wt;
                     q.push({distance[next],next});
                 }
             }
         }
-        int res=INT_MIN;
+        int res;
         for(int i=1;i<=n;i++){
             if(distance[i] == INT_MAX)return -1;
             res = max(res,distance[i]);
