ac1535b97d682c1b561e73e456d50187142707bf
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-06-01 17:46:06 +0530

    Time: 15 ms (76.12%) | Memory: 38.8 MB (35.91%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        vector<vector<int>> res(m, vector<int>(n, 0));
-        queue<pair<pair<int, int>, int>> q;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
+        int rows = mat.size(), cols = mat[0].size();
+        vector<vector<int>>visited(rows,vector<int>(cols,0));
+        queue<pair<int, pair<int, int>>> q;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0){
+                    q.push({0, {i, j}});
                     visited[i][j] = 1;
                 }
             }
         }
         while (!q.empty()) {
-            int dist = q.front().second;
-            int row = q.front().first.first;
-            int col = q.front().first.second;
+            int dist = q.front().first, row = q.front().second.first,
+                col = q.front().second.second;
             q.pop();
-            res[row][col] = dist;
-
             // up
-            if (row - 1 >= 0 && mat[row - 1][col] == 1 &&
-                !visited[row - 1][col]) {
-                q.push({{row - 1, col}, dist+1});
+            if (row - 1 >= 0 && !visited[row - 1][col] &&
+                mat[row - 1][col] == 1) {
+                q.push({dist + 1, {row - 1, col}});
                 visited[row - 1][col] = 1;
+                mat[row - 1][col] = dist + 1;
             }
-            // Right
-            if (col + 1 < n && mat[row][col + 1] == 1 &&
-                !visited[row][col + 1]) {
-                q.push({{row, col + 1}, dist + 1});
-                visited[row][col + 1] = 1;
-            }
-
-            // Left
-            if (col - 1 >= 0 && mat[row][col - 1] == 1 &&
-                !visited[row][col - 1]) {
-                q.push({{row, col - 1}, dist + 1});
-                visited[row][col - 1] = 1;
-            }
-
-            // Down
-            if (row + 1 < m && mat[row + 1][col] == 1 &&
-                !visited[row + 1][col]) {
-                q.push({{row + 1, col}, dist + 1});
+            if (row + 1 < rows && !visited[row + 1][col] &&
+                mat[row + 1][col] == 1) {
+                q.push({dist + 1, {row + 1, col}});
                 visited[row + 1][col] = 1;
+                mat[row + 1][col] = dist + 1;
+            }
+            if (col - 1 >= 0 && !visited[row][col - 1] &&
+                mat[row][col - 1] == 1) {
+                q.push({dist + 1, {row, col - 1}});
+                visited[row][col - 1] = 1;
+                mat[row][col - 1] = dist + 1;
+            }
+            if (col + 1 < cols && !visited[row][col + 1] &&
+                mat[row][col + 1] == 1) {
+                q.push({dist + 1, {row, col + 1}});
+                visited[row][col + 1] = 1;
+                mat[row][col + 1] = dist + 1;
             }
         }
-        return res;
+        return mat;
     }
 };
