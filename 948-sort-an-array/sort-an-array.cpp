88ae61762170ac78fa0e61b349785467febaec15
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2025-12-26 11:09:01 +0530

    Time: 610 ms (25.47%) | Memory: 295.6 MB (21.40%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    void mergeSort(vector<int>&arr,int left,int right){
-        if(left>=right)return;
-        int mid = left+(right-left)/2;
-        mergeSort(arr,left,mid);
-        mergeSort(arr,mid+1,right);
-        merge(arr,left,mid,right);
+    void mergeSort(vector<int>& arr, int left, int right) {
+        if (left >= right)
+            return;
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr, left, mid, right);
     }
-    void merge(vector<int>&arr,int left,int mid,int right){
-        vector<int>temp;
-        int l = left,r=mid+1;
-        while(l<=mid && r<=right){
-            if(arr[l]<arr[r]){
+    void merge(vector<int>& arr, int left, int mid, int right) {
+        vector<int> temp;
+        int l = left, r = mid + 1;
+        while (l <= mid && r <= right) {
+            if (arr[l] < arr[r]) {
                 temp.push_back(arr[l]);
                 l++;
-            }
-            else{
+            } else {
                 temp.push_back(arr[r]);
                 r++;
             }
         }
-        while(l<=mid){
+        while (l <= mid) {
             temp.push_back(arr[l]);
             l++;
         }
-        while(r<=right){
+        while (r <= right) {
             temp.push_back(arr[r]);
             r++;
         }
+        int n= temp.size();
         for(int i=left;i<=right;i++){
             arr[i]=temp[i-left];
         }
