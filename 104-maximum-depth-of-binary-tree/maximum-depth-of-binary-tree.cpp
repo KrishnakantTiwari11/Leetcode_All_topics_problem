b6b43cc6ab6ff38deff315106ae120e30c9484bd
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2026-01-02 23:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (12.52%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,18 +6,16 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int helper(TreeNode*node){
-        if(node==NULL)return 0;
-        int left=helper(node->left);
-        int right=helper(node->right);
-        return max(left,right)+1;
+    int helper(TreeNode* node) {
+        if (!node)
+            return 0;
+        return max(helper(node->left), helper(node->right)) + 1;
     }
-    int maxDepth(TreeNode* root) {
-        return helper(root);
-    }
+    int maxDepth(TreeNode* root) { return helper(root); }
 };
