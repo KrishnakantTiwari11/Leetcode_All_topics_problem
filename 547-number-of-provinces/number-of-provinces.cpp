ebfdb611bcdb12c80aa5afb13b21bad09327b370
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2026-01-06 21:28:28 +0530

    Time: 11 ms (6.75%) | Memory: 21.3 MB (9.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& adj,vector<int>&visited,int start){
-        visited[start]=1;
-        int n=adj.size();
-        for(int i=0;i<n;i++){
-            if(visited[i]==0 && adj[start][i]==1){
-                helper(adj,visited,i);
+    void dfs(vector<vector<int>>& isConnected, vector<int>& visited, int curr) {
+        visited[curr] = 1;
+        for (auto neigh : isConnected[curr]) {
+            if (!visited[neigh]) {
+                dfs(isConnected, visited, neigh);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<int>visited(n,0);
-        int res=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]==0){
+        int res = 0;
+        int V = isConnected.size();
+        vector<vector<int>> edges(V + 1);
+        vector<int> visited(V+1, 0);
+        for (int i = 0; i < isConnected.size(); i++) {
+            for (int j = 0; j < isConnected[i].size(); j++) {
+                if (isConnected[i][j]) {
+                    edges[i + 1].push_back(j + 1);
+                    edges[j + 1].push_back(i + 1);
+                }
+            }
+        }
+        for (int i = 1; i <=V; i++) {
+            if (!visited[i]) {
                 res++;
-                helper(isConnected,visited,i);
+                dfs(edges, visited, i);
             }
         }
         return res;
