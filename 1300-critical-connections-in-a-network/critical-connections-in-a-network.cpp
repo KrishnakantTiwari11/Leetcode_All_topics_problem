b95b7fd0dc05fa9ac189fa6e263cd846f9f4d762
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2026-01-29 00:34:25 +0530

    Time: 228 ms (24.00%) | Memory: 183.9 MB (31.38%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,32 +1,38 @@
 class Solution {
 private:
-int currTime = 1;
+    int currTime = 1;
+
 public:
-void helper(vector<vector<int>>&adj,vector<vector<int>>&res,vector<int>&tin,vector<int>&low,int parent,int node,vector<int>&visited){
-    visited[node]=1;
-    tin[node]=low[node]=currTime;
-    currTime++;
-    for(auto neigh:adj[node]){
-    if(neigh==parent)continue;
-    if(!visited[neigh]){
-    helper(adj,res,tin,low,node,neigh,visited);
-      low[node]=min(low[neigh],low[node]);
-      if(tin[node]<low[neigh]){
-      res.push_back({node,neigh});}
-    }else{
-      low[node]=min(low[neigh],low[node]);
-      }
-    }
-}
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>adj(n),res;
-        vector<int>tin(n),low(n),visited(n,0);
-        for(auto edge:connections){
-        int u=edge[0],v=edge[1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+    void helper(vector<vector<int>>& adj, vector<vector<int>>& res,
+                vector<int>& tin, vector<int>& low, vector<int>& visited,
+                int node, int parent) {
+        visited[node] = 1;
+        tin[node] = low[node] = currTime;
+        currTime++;
+        for (auto neigh : adj[node]) {
+            if (neigh == parent)
+                continue;
+            if (!visited[neigh]) {
+                helper(adj, res, tin, low, visited, neigh, node);
+                low[node] = min(low[node], low[neigh]);
+                if(tin[node]<low[neigh]){
+                    res.push_back({neigh,node});
+                }
+            } else {
+                low[node] = min(low[node], low[neigh]);
+            }
         }
-        helper(adj,res,tin,low,-1,0,visited);
+    }
+    vector<vector<int>> criticalConnections(int n,
+                                            vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n), res;
+        for (auto edge : connections) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> tin(n), low(n), visited(n);
+        helper(adj, res, tin, low, visited, 0, -1);
         return res;
     }
 };
