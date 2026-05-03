5e89ca2e618517fcd1174d6246574ad3da4b74a9
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2026-05-03 17:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.87%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -4,12 +4,6 @@ public:
         if(s==goal)return true;
         int n = s.length();
         if(n!=goal.length())return false;
-        for(int i=0;i<n-1;i++){
-            string temp = s.substr(1);
-            temp+=s[0];
-            s=temp;
-            if(s==goal)return true;
-        }
-        return false;
+        return(s+s).find(goal)!=-1;
     }
 };
