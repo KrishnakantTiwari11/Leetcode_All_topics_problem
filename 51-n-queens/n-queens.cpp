061f6503c58af9ea7b6475830837d4e77b2f917d
KrishnakantTiwari11 <155090396+KrishnakantTiwari11@users.noreply.github.com>
2026-05-05 10:29:47 +0530

    Time: 2 ms (72.66%) | Memory: 12.1 MB (17.78%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,32 +1,51 @@
 class Solution {
 public:
-    void solve(vector<vector<string>>& res, vector<string>& temp, int col, int n,vector<int> & left_row,vector<int> & low_diag,vector<int>& up_diag) {
-        if (col >= n) {
-            res.push_back(temp);
+    bool isSafe(vector<vector<string>>&board, int row, int col,
+                vector<int>& rowConfig,int n) {
+        if (rowConfig[row])
+            return false;
+             int i = row, j = col;
+    while (i >= 0 && j >= 0) {
+        if (board[i][j] == "Q") return false;
+        i--; j--;
+    }
+    i = row; j = col;
+    while (i < n && j >= 0) {
+        if (board[i][j] == "Q") return false;
+        i++; j--;
+    }
+
+    return true;
+    }
+    void solve(vector<vector<string>>& board, vector<vector<string>>& res,
+               int n, int col,vector<int>&rowConfig) {
+        if (col == n) {
+            vector<string> resTemp;
+            for (int i = 0; i < n; i++) {
+                string temp = "";
+                for (int j = 0; j < n; j++) {
+                    temp += board[i][j];
+                }
+                resTemp.push_back(temp);
+            }
+            res.push_back(resTemp);
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (left_row[row] == 0 && low_diag[row + col] == 0 &&
-                up_diag[n - 1 + col - row] == 0) {
-                temp[row][col] = 'Q';
-                left_row[row] = 1;
-                low_diag[row + col] = 1;
-                up_diag[n - 1 + col - row] = 1;
-                solve(res, temp, col+1, n,left_row,low_diag,up_diag);
-                left_row[row] = 0;
-                low_diag[row + col] = 0;
-                up_diag[n - 1 + col - row] = 0;
-                temp[row][col] = '.';
+        for (int i = 0; i < n; i++) {
+            if (isSafe(board, i, col, rowConfig,n)) {
+                rowConfig[i] = 1;
+                board[i][col] = "Q";
+                solve(board, res, n, col + 1,rowConfig);
+                board[i][col] = ".";
+                rowConfig[i] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>temp(n,string(n,'.'));
-        vector<vector<string>>res;
-        vector<int>left_row(n);
-        vector<int>low_diag(2*n-1);
-        vector<int>up_diag(2*n-1);
-        solve(res,temp,0,n,left_row,low_diag,up_diag);
+        vector<vector<string>> res;
+        vector<vector<string>> board(n, vector<string>(n, "."));
+        vector<int> rowConfig(n, 0);
+        solve(board, res, n, 0, rowConfig);
         return res;
     }
 };
